0060449afbcba1427653ef4ac0c4495f697de3aa
nageshwarp1997 <nageshwar@ctruh.com>
2024-12-30 21:58:30 +0530

    Added Routes

--- client/frontend/src/App.tsx
+++ client/frontend/src/App.tsx
@@ -1,5 +1,11 @@
+import { Outlet } from "react-router-dom";
+
 function App() {
-  return <div className="bg-red-600">App</div>;
+  return (
+    <div className="bg-primary">
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;
--- client/frontend/src/main.tsx
+++ client/frontend/src/main.tsx
@@ -1,10 +1,11 @@
 // import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "./styles.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/index.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   // <StrictMode>
-    <App />
+  <RouterProvider router={router} />
   // </StrictMode>,
-)
+);
