65b690fdff947bb02bc6e64a8a9916c2f7f2711b
nageshwarp1997 <nageshwar@ctruh.com>
2025-01-06 23:11:28 +0530

    dark mode button added for test

--- client/frontend/src/App.tsx
+++ client/frontend/src/App.tsx
@@ -1,8 +1,12 @@
 import { Outlet } from "react-router-dom";
+import DarkMode from "./components/DarkMode";
 
 function App() {
   return (
-    <div className="bg-primary">
+    <div className="bg-primary relative">
+      <div className="absolute top-4 right-4 bg-red-500 p-4 z-10 rounded-full">
+        <DarkMode />
+      </div>
       <Outlet />
     </div>
   );
