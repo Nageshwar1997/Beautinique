56724984a9b889440ac14ff79c24ac228f4b73aa
nageshwarp1997 <nageshwar@ctruh.com>
2025-01-12 16:47:04 +0530

    Input props added

--- frontend/client/src/types/index.tsx
+++ frontend/client/src/types/index.tsx
@@ -1,1 +1,20 @@
+import { ChangeEvent, KeyboardEvent, ReactNode } from "react";
+
 export type ThemeTypes = "light" | "dark";
+
+export interface InputProps {
+  label?: string;
+  placeholder?: string;
+  type?: "text" | "email" | "password";
+  name?: string;
+  errorText?: string;
+  successText?: string;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+  value?: string;
+  className?: string;
+  register?: object;
+  onKeyDown?: (e: KeyboardEvent<HTMLInputElement>) => void;
+  defaultValue?: string;
+  labelClassName?: string;
+  icon?: ReactNode;
+}
