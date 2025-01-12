04b52fc8c8fcb51f56e839de9be019b6a90da2e6
nageshwarp1997 <nageshwar@ctruh.com>
2025-01-12 14:33:52 +0530

    Auth Robot styles updated

--- frontend/client/src/pages/auth/components/AuthRobot.tsx
+++ frontend/client/src/pages/auth/components/AuthRobot.tsx
@@ -3,8 +3,17 @@ import Branding from "../../../components/Branding";
 
 const AuthRobot = () => {
   return (
-    <div className="w-1/2 relative hidden md:block rounded-2xl text-center bg-[url(./images/auth/bg-gradient.svg)] bg-center bg-cover bg-no-repeat overflow-hidden font-metropolis">
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-full w-full flex flex-col items-center justify-center">
+    <div className="w-1/2 hidden relative md:block rounded-2xl text-center bg-[url(./images/auth/bg-gradient.svg)] bg-center bg-cover bg-no-repeat overflow-hidden">
+      <div className="w-full absolute top-[10vh] left-1/2 transform -translate-x-1/2 flex flex-col items-center gap-2 text-white">
+        <h1 className="text-xl md:text-2xl lg:text-3xl font-semibold">
+          Welcome to CTRUH Shop
+        </h1>
+        <h2 className="text-base md:text-lg lg:text-xl">
+          Create a CTRUH Account
+        </h2>
+        <Branding type="light" />
+      </div>
+      <div className="relative h-full flex flex-col items-center justify-center">
         <div
           className="w-full h-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 rounded-[25px] overflow-hidden animate-pulse mix-blend-overlay"
           style={{
@@ -16,15 +25,6 @@ const AuthRobot = () => {
           scene="https://prod.spline.design/EDAPwzBU58Csa1Gc/scene.splinecode"
           className="z-20"
         />
-      </div>
-      <div className="w-fit pt-[10vh] mx-auto flex flex-col items-center gap-2 text-white">
-        <h1 className="text-xl md:text-2xl lg:text-3xl font-semibold">
-          Welcome to CTRUH Shop
-        </h1>
-        <h2 className="text-base md:text-lg lg:text-xl">
-          Create a CTRUH Account
-        </h2>
-        <Branding type="light" />
       </div>
     </div>
   );
