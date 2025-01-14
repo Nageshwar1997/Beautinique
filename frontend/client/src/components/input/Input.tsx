60f405220e76c766b666429bbf8c8cd4d4cd934e
nageshwarp1997 <nageshwar@ctruh.com>
2025-01-14 18:55:49 +0530

    Input updated

--- frontend/client/src/components/input/Input.tsx
+++ frontend/client/src/components/input/Input.tsx
@@ -1,4 +1,3 @@
-import { useState, ChangeEvent } from "react";
 import { CheckMark, InfoIcon } from "../icons";
 import { InputProps } from "../../types";
 
@@ -15,13 +14,6 @@ const Input = ({
   placeholder = "",
   labelClassName = "",
 }: InputProps) => {
-  const [inputValue, setInputValue] = useState(value || "");
-
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setInputValue(e.target.value);
-    if (onChange) onChange(e);
-  };
-
   return (
     <div className="w-full space-y-1">
       <div className="relative min-h-10 lg:min-h-12">
@@ -31,8 +23,8 @@ const Input = ({
           type={type}
           id={name}
           name={name}
-          value={inputValue}
-          onChange={handleInputChange}
+          value={value}
+          onChange={onChange}
           placeholder={placeholder ? placeholder : " "}
           className={`peer w-full flex-grow outline-none focus:outline-none font-normal text-base overflow-hidden bg-smoke-eerie rounded-xl border border-primary-inverted-10 px-4 py-2 lg:py-3 2xl:py-4 text-primary-inverted ${
             icon && "pr-10"
@@ -43,7 +35,7 @@ const Input = ({
           <label
             htmlFor={name}
             className={`flex items-center justify-center text-base cursor-text absolute left-2 text-primary-inverted-50 leading-none ${
-              inputValue
+              value
                 ? "top-0 px-1 md:px-2 py-0.5 border border-primary-inverted-10 text-[10px] lg:text-xs rounded-xl bg-smoke-eerie"
                 : "top-1/2 border border-transparent"
             } transform -translate-y-1/2 transition-all duration-200 leading-none peer-focus:top-0 peer-focus:text-[10px] peer-focus:lg:text-xs peer-focus:leading-none peer-focus:px-1 peer-focus:md:px-2 peer-focus:py-0.5 peer-focus:border peer-focus:border-primary-inverted-10 peer-focus:bg-smoke-eerie peer-focus:rounded-xl ${labelClassName}`}
