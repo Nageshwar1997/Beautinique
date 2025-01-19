1d5c440caf604dd8206a6e39d6ecc71ba9d9aaf4
Nageshwar <nageshpawarpatil@gmail.com>
2025-01-20 03:02:45 +0530

    Upload Image Done

--- backend/src/controllers/uploadImage.controller.ts
+++ backend/src/controllers/uploadImage.controller.ts
@@ -2,29 +2,26 @@ import { NextFunction, Request, Response } from "express";
 import AppError from "../utils/AppError";
 
 const uploadProfilePicController = (
-    req: Request,
-    res: Response,
-    next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ) => {
-    try {
-        // const profilePicUrl = req.file?.path; // URL of the uploaded image
-        const profilePicUrl = req.file?.path; // URL of the uploaded image
+  try {
+    const profilePicUrl = req.file?.path; // URL of the uploaded image
 
-        if (!profilePicUrl) {
-            return next(new AppError("Profile picture not uploaded", 400));
-        }
-
-        res
-            .status(200)
-            .json({
-                success: true,
-                error: false,
-                message: "Profile picture uploaded successfully",
-                url: profilePicUrl,
-            });
-    } catch (error: any) {
-        next(new AppError(error.message, 500));
+    if (!profilePicUrl) {
+      return next(new AppError("Profile picture not uploaded", 400));
     }
+
+    res.status(200).json({
+      success: true,
+      error: false,
+      message: "Profile picture uploaded successfully",
+      imageUrl: profilePicUrl,
+    });
+  } catch (error: any) {
+    next(new AppError(error.message, 500));
+  }
 };
 
 export default uploadProfilePicController;
--- frontend/client/src/pages/home/Home.tsx
+++ frontend/client/src/pages/home/Home.tsx
@@ -1,28 +1,22 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import { CameraIcon } from "../../components/icons";
 import axios from "axios";
 
 const Home = () => {
-    const [selectedImage, setSelectedImage] = useState<File | null>(null);
-    const [previewUrl, setPreviewUrl] = useState<string | null>(null);
-  const [uploading, setUploading] = useState(false);
-  
-  const handleImageChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
+
+  const handleImageChange = async (e: ChangeEvent<HTMLInputElement>) => {
+    setImageUrl(null);
     const file = e.target.files?.[0];
     if (file) {
       // Set image preview
-      setSelectedImage(file);
       const preview = URL.createObjectURL(file);
-      setPreviewUrl(preview);
+      setImageUrl(preview);
 
       // Upload image to backend
       try {
-        setUploading(true);
-
         const formData = new FormData();
         formData.append("profilePic", file);
-        formData.append("folderName", "profile_pics"); // Specify folder name
-        formData.append("fileName", "TestFileName"); // Specify file name
 
         const response = await axios.post(
           "http://localhost:8080/api/upload/profile-pic", // Replace with your backend URL
@@ -34,23 +28,19 @@ const Home = () => {
           }
         );
 
-        console.log("response", response);
-
-        // const { imageUrl } = response.data;
-        // console.log("Uploaded Image URL:", imageUrl);
+        const responseData = response?.data;
 
         // Update the profile picture URL (you can save this to state or context)
-        // setPreviewUrl(imageUrl);
+        if (responseData?.success) {
+          setImageUrl(responseData?.imageUrl);
+        }
       } catch (error) {
+        setImageUrl(null);
         console.error("Error uploading image:", error);
-      } finally {
-        setUploading(false);
       }
     }
   };
 
-  console.log("previewUrl", previewUrl);
-  console.log("selectedImage", selectedImage);
   return (
     <div className="w-20 h-20 mx-auto rounded-full relative shadow-2xl shadow-[blue] overflow-hidden group cursor-pointer">
       <label
@@ -59,12 +49,12 @@ const Home = () => {
       />
       <img
         src={
-          previewUrl ||
+          imageUrl ||
           "https://ctruhcdn.azureedge.net/main-webiste/public/images/products/individuals/ctruh-platfrom/categories/character/image10.webp"
         }
         alt="Profile Picture"
         className="object-cover bg-accent-duo rounded-full w-full h-full shadow-inner border-2 border-smoke-eerie"
-        // loading="lazy"
+        loading="lazy"
       />
       <div className="absolute bottom-0 left-0 right-0 bg-smoke-eerie flex items-center justify-center">
         <CameraIcon className="fill-primary-inverted opacity-70 group-hover:opacity-100" />
@@ -76,7 +66,6 @@ const Home = () => {
           onChange={handleImageChange}
         />
       </div>
-      {uploading && <p className="text-sm text-primary">Uploading...</p>}
     </div>
   );
 };
