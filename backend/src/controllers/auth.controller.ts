d4740346580f0411c8ef5e820c252298491bfdf5
Nageshwar <nageshpawarpatil@gmail.com>
2025-01-19 15:43:31 +0530

    Register user API Integration done

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -2,12 +2,6 @@ import bcrypt from "bcryptjs";
 import { Request, Response, NextFunction } from "express";
 import User from "../models";
 import generateToken from "../providers/jwt.provider";
-import AppError from "../utils/AppError";
-import {
-  isValidEmail,
-  isValidName,
-  isValidPassword,
-} from "../validators/user.validator";
 
 const registerController = async (
   req: Request,
@@ -15,57 +9,8 @@ const registerController = async (
   next: NextFunction
 ) => {
   try {
-    const { firstName, lastName, email, password, profileImage, DOB } =
+    const { firstName, lastName, email, password, profilePic,phoneNumber } =
       req.body;
-
-    if (!firstName) {
-      return next(new AppError("First name is required", 400));
-    }
-
-    if (firstName && !isValidName(firstName)) {
-      return next(
-        new AppError("First name must contain only letters and one space", 400)
-      );
-    }
-
-    if (!lastName) {
-      return next(new AppError("Last name is required", 400));
-    }
-
-    if (lastName && !isValidName(lastName)) {
-      return next(
-        new AppError("Last name must contain only letters and one space", 400)
-      );
-    }
-
-    if (!email) {
-      return next(new AppError("Email is required", 400));
-    }
-
-    if (email && !isValidEmail(email)) {
-      return next(new AppError("Enter a valid email address", 400));
-    }
-
-    if (!password) {
-      return next(new AppError("Password is required", 400));
-    }
-
-    if (password && !isValidPassword(password)) {
-      return next(
-        new AppError(
-          "Password must be at least 6 characters long and contain at least one letter, one number, and one special character",
-          400
-        )
-      );
-    }
-
-    const isUserExist = await User.countDocuments({ email: email.trim() });
-
-    if (isUserExist) {
-      return next(
-        new AppError(`User already exists with this email ${email.trim()}`, 400)
-      );
-    }
 
     const hashPassword = await bcrypt.hash(password.trim(), 10);
 
@@ -74,8 +19,9 @@ const registerController = async (
       firstName: firstName.trim(),
       lastName: lastName.trim(),
       email: email.trim(),
-      profileImage,
+      phoneNumber: phoneNumber.trim(),
       password: hashPassword,
+      profilePic,
     });
 
     const token = await generateToken(user?._id, next);
@@ -89,7 +35,7 @@ const registerController = async (
         firstName: user.firstName,
         lastName: user.lastName,
         email: user.email,
-        profileImage: user.profileImage,
+        profilePic: user.profilePic,
         role: user.role,
       },
     });
