2dc99d33e1ec291ae438128489aa5df6d310f58f
nageshwarp1997 <nageshwar@ctruh.com>
2025-01-29 21:42:52 +0530

    Loading Component Updated

--- frontend/client/src/components/Loaders/Loading/Loading.tsx
+++ frontend/client/src/components/Loaders/Loading/Loading.tsx
@@ -1,24 +1,26 @@
 import "./Loading.css";
 
-const Loading = () => {
+const Loading = ({
+  className,
+  content = "Loading....", // make sure the content length is not bigger than 11
+}: {
+  className?: string;
+  content: string;
+}) => {
   return (
-    <div className="container">
-      <div className="rings"></div>
-      <div className="rings"></div>
-      <div className="rings"></div>
-      <div className="rings"></div>
+    <div className={`container ${className}`}>
+      {Array.from({ length: 4 }).map((_, index) => (
+        <div key={index} className="rings" />
+      ))}
       <div className="absolute loading-text">
-        <span>L</span>
-        <span>O</span>
-        <span>A</span>
-        <span>D</span>
-        <span>I</span>
-        <span>N</span>
-        <span>G</span>
-        <span>.</span>
-        <span>.</span>
-        <span>.</span>
-        <span>.</span>
+        {content
+          .split("")
+          .splice(0, 11)
+          .map((char, index) => (
+            <span key={index} style={{ animationDelay: `${index * 0.1}s` }}>
+              {char}
+            </span>
+          ))}
       </div>
     </div>
   );
