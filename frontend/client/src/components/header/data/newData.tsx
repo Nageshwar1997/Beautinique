e7a94693a3cac042c2a7fe59d6df1b8080df2c5f
nageshwarp1997 <nageshwar@ctruh.com>
2025-02-09 13:22:05 +0530

    new Data added

---
+++ frontend/client/src/components/header/data/newData.tsx
@@ -0,0 +1,494 @@
+const for_you = {
+  id: 1,
+  level: 1,
+  label: "For You",
+  category: "for_you",
+  subCategories: [
+    {
+      id: 1,
+      level: 2,
+      label: "New",
+      category: "new",
+      subCategories: [
+        {
+          id: 1,
+          level: 3,
+          label: "",
+          category: "",
+        },
+      ],
+    },
+    {
+      id: 2,
+      level: 2,
+      label: "Sugar Play",
+      category: "sugar_play",
+      subCategories: [
+        {
+          id: 1,
+          level: 3,
+          label: "",
+          category: "",
+        },
+      ],
+    },
+    {
+      id: 3,
+      level: 2,
+      label: "Offers",
+      category: "offers",
+      subCategories: [
+        {
+          id: 1,
+          level: 3,
+          label: "",
+          category: "",
+        },
+      ],
+    },
+    {
+      id: 4,
+      level: 2,
+      label: "Blogs",
+      category: "blogs",
+      subCategories: [
+        {
+          id: 1,
+          level: 3,
+          label: "",
+          category: "",
+        },
+      ],
+    },
+  ],
+};
+
+const others = {
+  id: 2,
+  level: 1,
+  label: "Others",
+  category: "others",
+  subCategories: [
+    {
+      id: 1,
+      level: 2,
+      label: "Gifting",
+      category: "gifting",
+      subCategories: [
+        {
+          id: 1,
+          level: 3,
+          label: "",
+          category: "",
+        },
+      ],
+    },
+    {
+      id: 2,
+      level: 2,
+      label: "Sugar Pop",
+      category: "sugar_pop",
+      subCategories: [
+        {
+          id: 1,
+          level: 3,
+          label: "",
+          category: "",
+        },
+      ],
+    },
+    {
+      id: 3,
+      level: 2,
+      label: "Body Care",
+      category: "body_care",
+      subCategories: [
+        {
+          id: 1,
+          level: 3,
+          label: "",
+          category: "",
+        },
+      ],
+    },
+    {
+      id: 4,
+      level: 2,
+      label: "Hair Care",
+      category: "hair_care",
+      subCategories: [
+        {
+          id: 1,
+          level: 3,
+          label: "",
+          category: "",
+        },
+      ],
+    },
+  ],
+};
+
+const skin_and_face = {
+  id: 3,
+  level: 1,
+  label: "Skin & Face",
+  category: "skin_and_face",
+  subCategories: [
+    // Skin
+    {
+      id: 1,
+      level: 2,
+      label: "Moisturizers",
+      category: "moisturizers",
+      subCategories: [
+        {
+          id: 1,
+          level: 3,
+          label: "",
+          category: "",
+        },
+      ],
+    },
+    {
+      id: 2,
+      level: 2,
+      label: "Sun Protection",
+      category: "sun_protection",
+      subCategories: [
+        {
+          id: 1,
+          level: 3,
+          label: "",
+          category: "",
+        },
+      ],
+    },
+    {
+      id: 3,
+      level: 2,
+      label: "Skin Care Kits",
+      category: "skin_care_kits",
+      subCategories: [
+        {
+          id: 1,
+          level: 3,
+          label: "",
+          category: "",
+        },
+      ],
+    },
+    {
+      id: 4,
+      level: 2,
+      label: "Cleansing & Exfoliation",
+      category: "cleansing_and_exfoliation",
+      subCategories: [
+        {
+          id: 1,
+          level: 3,
+          label: "",
+          category: "",
+        },
+      ],
+    },
+    {
+      id: 5,
+      level: 2,
+      label: "Face Masks",
+      category: "face_masks",
+      subCategories: [
+        {
+          id: 1,
+          level: 3,
+          label: "",
+          category: "",
+        },
+      ],
+    },
+    {
+      id: 6,
+      level: 2,
+      label: "Natures Blend",
+      category: "natures_blend",
+      subCategories: [
+        {
+          id: 1,
+          level: 3,
+          label: "",
+          category: "",
+        },
+      ],
+    },
+    // Face
+    {
+      id: 7,
+      level: 2,
+      label: "Face Makeup",
+      category: "face_makeup",
+      subCategories: [
+        {
+          id: 1,
+          level: 3,
+          label: "",
+          category: "",
+        },
+      ],
+    },
+    {
+      id: 8,
+      level: 2,
+      label: "Setting & Finishing",
+      category: "setting_and_finishing",
+      subCategories: [
+        {
+          id: 1,
+          level: 3,
+          label: "",
+          category: "",
+        },
+      ],
+    },
+    {
+      id: 9,
+      level: 2,
+      label: "Foundation Types",
+      category: "foundation_types",
+      subCategories: [
+        {
+          id: 1,
+          level: 3,
+          label: "",
+          category: "",
+        },
+      ],
+    },
+    {
+      id: 10,
+      level: 2,
+      label: "Cheeks & Glows",
+      category: "cheeks_and_glows",
+      subCategories: [
+        {
+          id: 1,
+          level: 3,
+          label: "",
+          category: "",
+        },
+      ],
+    },
+    {
+      id: 11,
+      level: 2,
+      label: "Makeup Removers & Fixers",
+      category: "makeup_removers_and_fixers",
+      subCategories: [
+        {
+          id: 1,
+          level: 3,
+          label: "",
+          category: "",
+        },
+      ],
+    },
+    {
+      id: 12,
+      level: 2,
+      label: "Traditional & Essentials",
+      category: "traditional_and_essentials",
+      subCategories: [
+        {
+          id: 1,
+          level: 3,
+          label: "",
+          category: "",
+        },
+      ],
+    },
+    {
+      id: 13,
+      level: 2,
+      label: "Primers",
+      category: "primers",
+      subCategories: [
+        {
+          id: 1,
+          level: 3,
+          label: "",
+          category: "",
+        },
+      ],
+    },
+    {
+      id: 14,
+      level: 2,
+      label: "Bronzers & Contour",
+      category: "bronzers_and_contour",
+      subCategories: [
+        {
+          id: 1,
+          level: 3,
+          label: "",
+          category: "",
+        },
+      ],
+    },
+    {
+      id: 15,
+      level: 2,
+      label: "Concealers & Correctors",
+      category: "concealers_and_correctors",
+      subCategories: [
+        {
+          id: 1,
+          level: 3,
+          label: "",
+          category: "",
+        },
+      ],
+    },
+  ],
+};
+
+const lips_and_eyes = {
+  id: 4,
+  label: "Lips & Eyes",
+  category: "lips_and_eyes",
+  level: 1,
+  subcategories: [
+    // Lips
+    {
+      id: 1,
+      label: "Lipsticks",
+      category: "lipsticks",
+      level: 2,
+      subcategories: [
+        {
+          id: 1,
+          label: "",
+          category: "",
+          level: 3,
+        },
+      ],
+    },
+    {
+      id: 2,
+      label: "Lip Care",
+      category: "lip_care",
+      level: 2,
+      subcategories: [
+        {
+          id: 1,
+          label: "",
+          category: "",
+          level: 3,
+        },
+      ],
+    },
+    {
+      id: 3,
+      label: "Lipstick Sets & Combos",
+      category: "lipstick_sets_and_combos",
+      level: 2,
+      subcategories: [
+        {
+          id: 1,
+          label: "",
+          category: "",
+          level: 3,
+        },
+      ],
+    },
+    {
+      id: 4,
+      label: "Kohl & Kajal",
+      category: "kohl_and_kajal",
+      level: 2,
+      subcategories: [
+        {
+          id: 1,
+          label: "",
+          category: "",
+          level: 3,
+        },
+      ],
+    },
+    {
+      id: 5,
+      label: "Mascara",
+      category: "mascara",
+      level: 2,
+      subcategories: [
+        {
+          id: 1,
+          label: "",
+          category: "",
+          level: 3,
+        },
+      ],
+    },
+    {
+      id: 6,
+      label: "Eye Liner",
+      category: "eye_liner",
+      level: 2,
+      subcategories: [
+        {
+          id: 1,
+          label: "",
+          category: "",
+          level: 3,
+        },
+      ],
+    },
+    {
+      id: 7,
+      label: "Eye Shadow",
+      category: "eye_shadow",
+      level: 2,
+      subcategories: [
+        {
+          id: 1,
+          label: "",
+          category: "",
+          level: 3,
+        },
+      ],
+    },
+    {
+      id: 8,
+      label: "Eye Brow",
+      category: "eye_brow",
+      level: 2,
+      subcategories: [
+        {
+          id: 1,
+          label: "",
+          category: "",
+          level: 3,
+        },
+      ],
+    },
+    {
+      id: 8,
+      label: "Eye Value Sets",
+      category: "eye_value_sets",
+      level: 2,
+      subcategories: [
+        {
+          id: 1,
+          label: "Eyelashes",
+          category: "eyelashes",
+          level: 3,
+        },
+        {
+          id: 2,
+          label: "Eye Combos",
+          category: "eye_combos",
+          level: 3,
+        },
+      ],
+    },
+  ],
+};
+
+export const data = [for_you, others, skin_and_face, lips_and_eyes];
