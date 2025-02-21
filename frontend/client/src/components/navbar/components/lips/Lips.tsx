ba485287afab88897d1aed63ea5d5003600092f7
nageshwarp1997 <nageshwar@ctruh.com>
2025-02-21 12:35:44 +0530

    Added Lips

---
+++ frontend/client/src/components/navbar/components/lips/Lips.tsx
@@ -0,0 +1,74 @@
+import { MessageIcon } from "../../../../icons";
+import { lips } from "../../data/newData";
+
+const Lips = () => {
+  const categories = lips.subCategories;
+  return (
+    <div className="w-full space-y-4">
+      <div className="p-4 xl:p-0 grid sm:grid-cols-2 md:grid-cols-3 md:gap-4 lg:grid-cols-4 grid-rows-1 lg:gap-6 justify-center w-full">
+        {categories.map((category, index) => (
+          <div
+            key={index}
+            className="space-y-4 min-w-[200px] max-w-[300px] pb-4 lg:pb-0 border-b border-primary-battleship-davys-gray lg:border-none"
+          >
+            <p className="uppercase text-center lg:mt-0 text-primary-battleship-davys-gray-inverted text-base lg:text-sm font-semibold font-degular tracking-wide leading-5 px-3 cursor-pointer">
+              {category?.label}
+            </p>
+            <div className="flex flex-col gap-1 md:gap-2 lg:gap-3">
+              {category.subCategories.map((subCategory, index) => {
+                const isSerum = subCategory.label === "Hi-Shine Lipsticks";
+                // const Icon = subCategory.icon;
+                return (
+                  <div
+                    key={index}
+                    className={`flex justify-start gap-2 p-2 border border-transparent hover:bg-white-smoke-night-inverted rounded-xl cursor-pointer ${
+                      isSerum
+                        ? "hover:border-blue-crayola-c"
+                        : "hover:border-primary-8"
+                    } group`}
+                  >
+                    {/* <div
+                      className={`min-w-10 min-h-10 max-w-10 max-h-10 xl:min-w-14 xl:min-h-14 xl:max-w-14 xl:max-h-14 bg-secondary-inverted group-hover:bg-primary-inverted z-10 rounded-lg flex items-center justify-center ${
+                        isSerum
+                          ? "bg-accent-duo group-hover:shadow-primary-btn-hover [&>svg]:!fill-white opacity-100"
+                          : "shadow-inner shadow-primary-battleship-davys-gray"
+                      }`}
+                    >
+                      <Icon className="z-20 fill-secondary" />
+                    </div> */}
+                    <div className="flex flex-col justify-center lg:justify-start gap-1 w-full">
+                      <p className="text-xs xl:text-sm tracking-wide leading-3 text-left w-full">
+                        {subCategory.label}
+                      </p>
+                      {/* <p className="text-[8px] xl:text-[10px] leading-1 break-words line-clamp-2">
+                      {subCategory.description}
+                    </p> */}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        ))}
+      </div>
+      <hr className="hidden lg:block border border-primary-50" />
+      <div className="w-full hidden lg:block">
+        <div className="flex items-center gap-1">
+          <MessageIcon className="w-5 h-5 fill-secondary" />
+          <p className="text-sm font-normal leading-[21px] text-secondary">
+            Customer's Feedback:
+          </p>
+        </div>
+        <div className="text-tertiary text-xs mt-2">
+          Great variety of lipsticks! Loved the shades and textures.
+          Long-lasting and comfortable. Perfect for every occasion. Highly
+          recommended! Great variety of lipsticks! Loved the shades and
+          textures. Long-lasting and comfortable. Perfect for every occasion.
+          Highly recommended!
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Lips;
