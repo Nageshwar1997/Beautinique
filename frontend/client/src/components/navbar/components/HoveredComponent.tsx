5fb57c5138f6ecca16e39f07bed986f90ed50a4f
nageshwarp1997 <nageshwar@ctruh.com>
2025-02-24 22:18:56 +0530

    About Component Done

--- frontend/client/src/components/navbar/components/HoveredComponent.tsx
+++ frontend/client/src/components/navbar/components/HoveredComponent.tsx
@@ -8,8 +8,8 @@ const HoveredComponent = ({ index }: { index: number }) => {
   const Component = navbarCategoriesData[index].component;
 
   return (
-    <div className="lg:p-px lg:bg-primary-battleship-davys-gray lg:rounded-xl w-full h-full backdrop-blur-3xl max-w-[1300px] shadow-navbar-card">
-      <div className="bg-platinum-black text-secondary lg:w-full lg:p-5 lg:bg-secondary-inverted lg:rounded-xl lg:font-metropolis">
+    <div className="lg:p-px lg:bg-primary-battleship-davys-gray lg:rounded-xl h-full backdrop-blur-3xl max-w-[1300px] shadow-navbar-card">
+      <div className="bg-platinum-black text-secondary lg:p-5 lg:bg-secondary-inverted lg:rounded-xl lg:font-metropolis">
         <Component />
       </div>
     </div>
--- frontend/client/src/components/navbar/components/about/About.tsx
+++ frontend/client/src/components/navbar/components/about/About.tsx
@@ -1,7 +1,11 @@
 import { useEffect, useState } from "react";
 import Button from "../../../button/Button";
 import { careers, company, press, trust_center } from "./data";
-import { YellowStarIcon } from "../../../../icons";
+import {
+  LeftArrowIcon,
+  RightArrowIcon,
+  YellowStarIcon,
+} from "../../../../icons";
 
 interface Testimonial {
   id: number;
@@ -40,66 +44,56 @@ const TestimonialCarousel: React.FC<{ testimonials: Testimonial[] }> = ({
   };
 
   return (
-    <div className="bg-ctruh-neutral p-4 rounded-lg flex flex-col">
-      <div className="overflow-hidden">
+    <div
+      onMouseEnter={() => setIsPaused(true)}
+      onMouseLeave={() => setIsPaused(false)}
+      onClick={() => setIsPaused(!isPaused)}
+      className="bg-tertiary-inverted p-4 rounded-lg"
+    >
+      {testimonials.map((testimonial, index) => (
         <div
-          onMouseEnter={() => setIsPaused(true)}
-          onMouseLeave={() => setIsPaused(false)}
+          className={`w-full flex flex-col gap-2 group ${
+            index === currentIndex ? "block" : "hidden"
+          }`}
         >
-          {testimonials.map((testimonial, index) => (
-            <div
-              key={testimonial.id}
-              className={`w-full ${
-                index === currentIndex ? "block" : "hidden"
-              }`}
-            >
-              {/* container for the content */}
-              <div className="flex flex-col gap-3">
-                <div className="flex gap-1">
-                  {Array.from({ length: 5 }).map((_, index) => (
-                    <YellowStarIcon key={index} className="w-4 h-4" />
-                  ))}
-                </div>
-                <p className="text-primary base:h-[10vh] text-sm font-normal italic leading-[21px]">
-                  &quot;{testimonial.content}&quot;
+          <div className="flex flex-col gap-4 h-[115px] base:h-[128px] md:h-[150px] leading-6">
+            <div className="flex gap-1">
+              {Array.from({ length: 5 }).map((_, idx) => (
+                <YellowStarIcon key={idx} className="w-4 h-4 md:w-5 md:h-5" />
+              ))}
+            </div>
+            <p className="text-tertiary text-xs group-hover:text-primary md:text-sm font-normal italic">
+              &quot;{testimonial.content}&quot;
+            </p>
+          </div>
+          <div className="flex gap-2 justify-between">
+            <div className="flex items-center gap-2">
+              <img
+                src={testimonial.image}
+                alt={testimonial.name}
+                className="w-10 h-10 md:w-12 md:h-12 rounded-full object-cover"
+                loading="lazy"
+              />
+              <div className="flex flex-col">
+                <p className="text-secondary text-xs font-semibold">
+                  {testimonial.name}
                 </p>
-                <div className="flex items-center gap-2">
-                  <img
-                    src={testimonial.image}
-                    alt={testimonial.name}
-                    className="w-8 h-8 rounded-full flex-shrink-0 object-cover"
-                    loading="lazy"
-                  />
-                  <div className="flex items-center gap-2 justify-between w-full">
-                    <div>
-                      <p className="text-primary text-xs font-semibold leading-[18px]">
-                        {testimonial.name}
-                      </p>
-                      <p className="text-light-secondary text-[10px] font-normal leading-4">
-                        {testimonial.role}
-                      </p>
-                    </div>
-                    <div className="flex gap-2">
-                      <span
-                        onClick={handlePrev}
-                        className="cursor-pointer [&>path]:stroke-primary"
-                      >
-                        P
-                      </span>
-                      <span
-                        onClick={handleNext}
-                        className="cursor-pointer [&>path]:stroke-primary"
-                      >
-                        N
-                      </span>
-                    </div>
-                  </div>
-                </div>
+                <p className="text-tertiary text-[10px] font-normal leading-4">
+                  {testimonial.role}
+                </p>
               </div>
             </div>
-          ))}
+            <div className="flex justify-center gap-3 px-2">
+              <button onClick={handlePrev}>
+                <LeftArrowIcon className="fill-tertiary w-4 h-4" />
+              </button>
+              <button onClick={handleNext}>
+                <RightArrowIcon className="fill-tertiary w-4 h-4" />
+              </button>
+            </div>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
@@ -110,55 +104,48 @@ const Collections = () => {
 
   const categories = [colOneData, colTwoData];
 
-const testimonials = [
-  {
-    id: 1,
-    content:
-      "I absolutely love the range of products on this website! The quality is unmatched, and my skin has never felt better. I always get compliments!",
-    name: "Ananya Sharma",
-    role: "Beauty Enthusiast",
-    image:
-      "https://ctruhcdn.azureedge.net/main-webiste/public/images/navbar/krishna-gupta.webp",
-  },
-  {
-    id: 2,
-    content: `Finding the perfect shade was so easy. The product descriptions and customer reviews helped me make the right choice. Fast delivery too!`,
-    name: "Sofia Patel",
-    role: "Makeup Lover",
-    image:
-      "https://ctruhcdn.azureedge.net/main-webiste/public/images/navbar/aarav-joshi.webp",
-  },
-  {
-    id: 3,
-    content: `I love how the products feel on my skin. They are lightweight, long-lasting, and make me feel confident all day. Highly recommended!`,
-    name: "Maya Williams",
-    role: "Fashion Influencer",
-    image:
-      "https://ctruhcdn.azureedge.net/main-webiste/public/images/navbar/lucas-smith.webp",
-  },
-];
+  const testimonials = [
+    {
+      id: 1,
+      content:
+        "I absolutely love the range of products on this website! The quality is unmatched, and my skin has never felt better. I always get compliments!",
+      name: "Nageshwar Pawar",
+      role: "Founder",
+      image:
+        "https://ctruhcdn.azureedge.net/main-webiste/public/images/navbar/krishna-gupta.webp",
+    },
+    {
+      id: 2,
+      content: `Finding the perfect shade was so easy. The product descriptions and customer reviews helped me make the right choice. Fast delivery too!`,
+      name: "Manjusha Magar",
+      role: "CEO",
+      image:
+        "https://ctruhcdn.azureedge.net/main-webiste/public/images/navbar/aarav-joshi.webp",
+    },
+    {
+      id: 3,
+      content: `I love how the products feel on my skin. They are lightweight, long-lasting, and make me feel confident all day. Highly recommended!`,
+      name: "Sanket Pawar",
+      role: "Manager",
+      image:
+        "https://ctruhcdn.azureedge.net/main-webiste/public/images/navbar/lucas-smith.webp",
+    },
+  ];
 
   return (
-    <div className="p-4 lg:p-0 w-full h-full grid grid-cols-1 base:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 base:gap-3 md:gap-4 lg:gap-5 content-center">
+    <div className="p-4 lg:p-0 grid grid-cols-1 base:grid-cols-2 md:grid-cols-3 gap-2 base:gap-3 md:gap-4 lg:gap-5 justify-start content-start">
       {categories.map((item, i) => (
-        <div key={i} className={`flex flex-col gap-2 border`}>
+        <div key={i} className={`flex flex-col gap-2 w-fit`}>
           {item.map((category, index) => {
             return (
               <div
                 key={index}
-                className={`space-y-4 min-w-[200px] max-w-[300px] pb-4 lg:pb-0 border border-primary-battleship-davys-gray !border-[red] ${
-                  [
-                    "traditional_and_essentials",
-                    "setting_and_finishing",
-                    "foundation_by_skin_type",
-                    "concealers_and_correctors",
-                  ].includes(category.category)
+                className={`space-y-4 min-w-[200px] max-w-[300px] pb-4 lg:pb-0 border-b border-primary-battleship-davys-gray ${
+                  ["press", "trust_center_and_legal"].includes(
+                    category.category
+                  )
                     ? "lg:border-none"
                     : "lg:pb-2"
-                } ${
-                  category.category === "foundation_by_skin_type"
-                    ? "base:pb-6 md:pb-4"
-                    : ""
                 }`}
               >
                 <p className="uppercase mt-3 md:mt-0 text-primary-battleship-davys-gray-inverted text-base base:text-sm font-semibold font-degular tracking-wide leading-5 px-3 cursor-pointer line-clamp-1">
@@ -206,39 +193,27 @@ const testimonials = [
           })}
         </div>
       ))}
-      <div className="border base:col-span-2 flex flex-col gap-2">
-        <div className="w-full h-full lg:h-1/2 flex flex-col base:flex-row gap-5 border border-[red]">
-          <div className="w-full max-h-[310px] lg:w-1/2 space-y-4 border border-[blue]">
-            <p className="uppercase mt-3 md:mt-0 text-primary-battleship-davys-gray-inverted text-base base:text-sm font-semibold font-degular tracking-wide leading-5 px-3 cursor-pointer">
-              Peoples Love BQ
-            </p>
-            <TestimonialCarousel testimonials={testimonials} />
-          </div>
-          <div className="w-full lg:w-1/2 space-y-4 border border-[yellow]">
-            <p className="uppercase mt-3 md:mt-0 text-primary-battleship-davys-gray-inverted text-base base:text-sm font-semibold font-degular tracking-wide leading-5 px-3 cursor-pointer">
-              Partner with us
-            </p>
-            <div className="px-3 flex flex-col gap-4 items-start border border-[green]">
-              <span className="bg-accent-duo bg-clip-text text-transparent italic text-sm font-medium leading-[21px]">
-                Together, we're Unstoppable!
-              </span>
-              <Button
-                content={"Team Up"}
-                pattern="outline"
-                className="!w-fit !py-1 !rounded-full leading-3 !text-sm"
-              />
-            </div>
-          </div>
+      <div className="flex flex-col gap-2 w-fit">
+        <div className="space-y-4 min-w-[200px] max-w-[300px] pb-4 lg:pb-2 border-b border-primary-battleship-davys-gray">
+          <p className="uppercase mt-3 md:mt-0 text-primary-battleship-davys-gray-inverted text-base base:text-sm font-semibold font-degular tracking-wide leading-5 px-3 cursor-pointer line-clamp-1">
+            Peoples Love BQ
+          </p>
+          <TestimonialCarousel testimonials={testimonials} />
         </div>
-        <div className="w-full max-h-1/2 hidden lg:block min-h-[190px] xl:max-h-[260px]  shadow-lg shadow-secondary-inverted">
-          <span className="w-full h-full overflow-hidden">
-            <img
-              src="./images/navbar/Lipstick1.jpg"
-              alt=""
-              className="w-full h-full rounded-xl object-cover object-center opacity-95 hover:opacity-100 cursor-pointer"
-              loading="lazy"
+        <div className="grow space-y-4 min-w-[200px] max-w-[300px] pb-4 lg:pb-2 border-b border-primary-battleship-davys-gray lg:border-none">
+          <p className="uppercase mt-3 md:mt-0 text-primary-battleship-davys-gray-inverted text-base base:text-sm font-semibold font-degular tracking-wide leading-5 px-3 cursor-pointer">
+            Partner with us
+          </p>
+          <div className="px-3 space-y-4">
+            <span className="bg-accent-duo bg-clip-text text-transparent italic text-sm font-medium leading-[21px]">
+              Together, we're Unstoppable!
+            </span>
+            <Button
+              content={"Team Up"}
+              pattern="outline"
+              className="!w-fit !py-1 !rounded-full leading-3 !text-sm"
             />
-          </span>
+          </div>
         </div>
       </div>
     </div>
