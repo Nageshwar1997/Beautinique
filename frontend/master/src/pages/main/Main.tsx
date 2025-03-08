7f8d0d75c42ec1e7217ccc86e85a36eac3811828
nageshwarp1997 <nageshwar@ctruh.com>
2025-03-08 22:55:18 +0530

    Added Scroll gradient to bottom

--- frontend/master/src/pages/main/Main.tsx
+++ frontend/master/src/pages/main/Main.tsx
@@ -4,10 +4,14 @@ import Footer from "../../components/footer/Footer";
 import ErrorBoundary from "../error/ErrorBoundary";
 import { useUserStore } from "../../store/user.store";
 import { useGetUserDetails } from "../../api/user/user.service";
-import { useEffect } from "react";
+import { RefObject, useEffect } from "react";
 import Sidebar from "../../components/sidebar/Sidebar";
+import { VerticalScrollType } from "../../types";
+import { BottomGradient } from "../../components/Gradients";
+import useVerticalScrollable from "../../hooks/useVerticalScrollable";
 
 const Main = () => {
+  const [showGradient, containerRef] = useVerticalScrollable();
   const { isAuthenticated, setUser } = useUserStore();
   const { data } = useGetUserDetails();
   useEffect(() => {
@@ -18,18 +22,26 @@ const Main = () => {
 
   return (
     <ErrorBoundary>
-      <div className="max-h-dvh min-h-dvh max-w-full min-w-full w-dvw h-dvh flex gap-2 overflow-hidden p-2">
+      <div className="max-h-dvh min-h-dvh max-w-full min-w-full w-dvw h-dvh flex gap-3 overflow-hidden p-2">
         <Sidebar />
-        <div className="grow max-w-full h-full overflow-y-scroll">
-          <div className="bg-primary-inverted w-full sticky top-0 z-50">
-            <Navbar />
+        <div className="w-full h-full relative">
+          <div
+            ref={containerRef as RefObject<HTMLDivElement>}
+            className="grow max-w-full h-full overflow-y-scroll rounded-lg border relative"
+          >
+            <div className="bg-primary-inverted w-full sticky top-0 z-50">
+              <Navbar />
+            </div>
+            <div className="w-full">
+              <main className="w-full py-3">
+                <Outlet />
+              </main>
+              <Footer />
+            </div>
           </div>
-          <div className="w-full p-2">
-            <main className="w-full py-2">
-              <Outlet />
-            </main>
-            <Footer />
-          </div>
+          {(showGradient as VerticalScrollType).bottom && (
+            <BottomGradient className="!w-full h-8 from-secondary-inverted rounded-b-lg z-[100]" />
+          )}
         </div>
       </div>
     </ErrorBoundary>
